658487f550671661268c967b84be665b120bf576
Leopold Chow <chowleepok@hotmail.com>
2018-05-25 17:36:27 -0700

    added one test for Expressions/Scope Integration

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -405,7 +405,18 @@ describe('Scope', function() {
 
         expect(theValue).toBe(42);
       });
-      
+
+      it('accepts expressions for watch functions', function () {
+        var theValue;
+
+        scope.aColl = [1, 2, 3];
+        scope.$watchCollection('aColl', function(newValue, oldValue, scope) {
+          theValue = newValue;
+        });
+        scope.$digest();
+
+        expect(theValue).toEqual([1, 2, 3]);
+      });
   });
 
 
