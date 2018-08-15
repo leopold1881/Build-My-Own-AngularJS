e1476ba8ef80f0b68f9b2278a47540648be81775
Leopold Chow <chowleepok@hotmail.com>
2018-08-14 22:12:57 -0700

    Added error feature for circular dependency

--- src/injector.js
+++ src/injector.js
@@ -11,6 +11,7 @@ function createInjector(modulesToLoad, strictDi) {
   var providerCache = {};
   var instanceCache = {};
   var loadModules = {};
+  var path = [];
   strictDi = (strictDi === true);
 
   var $provide = {
@@ -71,14 +72,23 @@ function createInjector(modulesToLoad, strictDi) {
   function getService(name) {
     if (instanceCache.hasOwnProperty(name)) {
       if (instanceCache[name] === INSTANTIATING) {
-        throw new Error('Circular dependency found');
+        throw new Error('Circular dependency found: ' +
+          name + ' <- ' + path.join(' <- '));
       }
       return instanceCache[name];
     } else if (providerCache.hasOwnProperty(name + 'Provider')) {
+      path.unshift(name);
       instanceCache[name] = INSTANTIATING;
-      var provider = providerCache[name + 'Provider'];
-      var instance = instanceCache[name] = invoke(provider.$get);
-      return instance;
+      try {
+        var provider = providerCache[name + 'Provider'];
+        var instance = instanceCache[name] = invoke(provider.$get);
+        return instance;
+      } finally {
+        path.shift();
+        if (instanceCache[name] === INSTANTIATING) {
+          delete instanceCache[name];
+        }
+      }
     }
   }
 
